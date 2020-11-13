1464ff8c97ba70d0a0ea4662cf31f18863209d54
phedrakeson <gabrield.developer@gmail.com>
2020-11-13 11:00:10 -0300

    landing page

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
 
 app.use('/', express.static(__dirname+'/public'));
+app.use('/', express.static(__dirname+'/img'));
 
 app.get('/', (req, res) => {
   return res.render('home', { rooms });
